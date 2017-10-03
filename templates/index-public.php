6dd1a7415486f7926f7cee6a966128da9168799e
Daniel Calviño Sánchez <danxuliu@gmail.com>
2017-10-03 12:15:46 +0200

    Add rough sidebar for guests in public calls
    Just a copy of the markup for the sidebar from the index.php template
    (which includes more than what is really needed for the chat view).
    
    Signed-off-by: Daniel Calviño Sánchez <danxuliu@gmail.com>

--- templates/index-public.php
+++ templates/index-public.php
@@ -6,14 +6,18 @@ vendor_script('select2/select2');
 vendor_style('select2/select2');
 
 style('spreed', 'style');
+style('spreed', 'comments');
 script(
 	'spreed',
 	[
 		'vendor/backbone/backbone-min',
 		'vendor/backbone.radio/build/backbone.radio.min',
 		'vendor/backbone.marionette/lib/backbone.marionette.min',
+		'models/message',
+		'models/messagecollection',
 		'models/room',
 		'models/roomcollection',
+		'views/chatview',
 		'views/roomlistview',
 		'simplewebrtc',
 		'webrtc',
@@ -31,6 +35,44 @@ script(
 <div id="app" class="nc-enable-screensharing-extension" data-token="<?php p($_['token']) ?>">
 	<div id="app-content" class="participants-1">
 
+		<div id="app-sidebar" class="detailsView scroll-container hidden">
+			<div class="detailCallInfoContainer">
+				<h3><span class="room-name">Name</span></h3>
+				<!--
+				<h3>Call name <span class="icon icon-rename"></span></h3>
+
+				<button><?php p($l->t('Start/stop webinary'));?></button>
+
+				<input name="shareLink" id="shareLink" class="checkbox" value="1" type="checkbox">
+				<label for="shareLink"><?php p($l->t('Share link'));?></label><br>
+				<div class="oneline">
+					<label for="linkText" class="hidden-visually">Link</label>
+					<input id="linkText" class="linkText" type="text" readonly value="https://nextcloud13.local/index.php/s/LRDYjaFrAw2oBp7">
+					<a class="clipboardButton icon icon-clippy" data-clipboard-target="#linkText" data-original-title="" title=""></a>
+				</div>
+				-->
+			</div>
+
+			<ul class="tabHeaders">
+				<li class="tabHeader selected" data-tabid="participantTabView" data-tabindex="0">
+					<a href="#"><?php p($l->t('Participants'));?></a>
+				</li>
+				<!--<li class="tabHeader" data-tabid="schedulingTabView" data-tabindex="1">
+					<a href="#"><?php p($l->t('Scheduling'));?></a>
+				</li>-->
+			</ul>
+
+			<div class="tabsContainer">
+				<div id="participantTabView" class="tab participantTabView">
+					<div class="participantListView subView">
+						<ul id="participantWithList" class="participantWithList with-icon">
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="app-content-wrapper">
 		<header>
 			<div id="header" class="spreed-public">
 				<div id="header-left">
@@ -94,6 +136,7 @@ script(
 			<div id="emptycontent-icon" class="icon-video"></div>
 			<h2><?php p($l->t('Looking great today! :)')) ?></h2>
 			<p class="uploadmessage"><?php p($l->t('Smile in 3… 2… 1!')) ?></p>
+		</div>
 		</div>
 	</div>
 </div>
